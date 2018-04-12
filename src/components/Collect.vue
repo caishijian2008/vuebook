34677362a5c5707c8eb43d187cae5244d0495f48
caishijian2008 <442754043@qq.com>
2018-04-12 19:01:34 +0800

    购物车 css 1

--- src/components/Collect.vue
+++ src/components/Collect.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <MHeader>购物车</MHeader>
-    <ul class="content">
-      <li v-for="(cart, index) in cartList" :key="index">
-        <img :src="cart.bookCover">
-        <h3>{{cart.bookName}}</h3>
-        <button>-</button>{{cart.bookCount}}<button>+</button>
-        <p>小计：{{cart.bookPrice * cart.bookCount}}</p>
-        <button>删除</button>
-      </li>
-      <li>共{{count}}本</li>
-    </ul>
+    <div class="content">
+      <ul>
+        <li v-for="(cart, index) in cartList" :key="index">
+          <img :src="cart.bookCover">
+          <div>
+            <h3>{{cart.bookName}}</h3>
+            <button>-</button>{{cart.bookCount}}<button>+</button>
+            <p>小计：{{cart.bookPrice * cart.bookCount}}</p>
+            <button class="remove" @click.stop="">删除</button>
+          </div>
+        </li>
+        <li>共{{count}}本</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -33,6 +37,38 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.content {
+  ul {
+    padding: 10px;
+    li {
+      display: flex;
+      padding: 10px 0;
+      border-bottom: 1px solid #f1f1f1;
+      img {
+        width: 8em;
+        height: 9em;
+      }
+      p {
+        font-size: 18px;
+      }
+      button {
+        width: 60px;
+        height: 25px;
+        background: #ccc;
+        color: #000;
+        border: none;
+        border-radius: 5px;
+        outline: none;
+        margin: 5px;
+        text-align: center;
+        font-size: 18px;
+      }
+      .remove {
+        background-color: orangered;
+        color: #fff;
+      }
+    }
+  }
+}
 </style>
