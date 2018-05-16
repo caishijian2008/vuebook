5d8c2d1366f7004277ce193efddfbfd75e66dcbd
caishijian2008 <442754043@qq.com>
2018-05-16 16:52:56 +0800

    about 2

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,51 +1,27 @@
 <template>
   <div>
-    <MHeader>添加</MHeader>
+    <MHeader>我的</MHeader>
     <div class="content">
-      <ul>
-        <li>
-          <label for="bookName">书名</label>
-          <input type="text" v-model="book.bookName" id="bookName">
-        </li>
-        <li>
-          <label for="bookInfo">信息</label>
-          <input type="text" v-model="book.bookInfo" id="bookInfo">
-        </li>
-        <li>
-          <label for="bookPrice">价格</label>
-          <input type="number" v-model.number="book.bookPrice" id="bookPrice">
-        </li>
-        <li>
-          <label for="bookCover">封面</label>
-          <input type="text" v-model="book.bookCover" id="bookCover">
-        </li>
-        <li>
-          <button @click="add">添加</button>
-        </li>
-      </ul>
+      <div></div>
     </div>
   </div>
 </template>
 
 <script>
 import MHeader from '@/base/MHeader'
-import {addBook} from '../api'
 export default {
   created () {
     //
   },
   methods: {
-    async add () {
-      await addBook(this.book)
-      this.$router.push('/list')
-    }
+    //
   },
   components: {
     MHeader
   },
   data () {
     return {
-      book: {}
+      //
     }
   }
 }
---
+++ src/components/Add.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <MHeader>添加</MHeader>
+    <div class="content">
+      <ul>
+        <li>
+          <label for="bookName">书名</label>
+          <input type="text" v-model="book.bookName" id="bookName">
+        </li>
+        <li>
+          <label for="bookInfo">信息</label>
+          <input type="text" v-model="book.bookInfo" id="bookInfo">
+        </li>
+        <li>
+          <label for="bookPrice">价格</label>
+          <input type="number" v-model.number="book.bookPrice" id="bookPrice">
+        </li>
+        <li>
+          <label for="bookCover">封面</label>
+          <input type="text" v-model="book.bookCover" id="bookCover">
+        </li>
+        <li>
+          <button @click="add">添加</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import MHeader from '@/base/MHeader'
+import {addBook} from '../api'
+export default {
+  created () {
+    //
+  },
+  methods: {
+    async add () {
+      await addBook(this.book)
+      this.$router.push('/list')
+    }
+  },
+  components: {
+    MHeader
+  },
+  data () {
+    return {
+      book: {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  ul {
+    margin: 3.13rem 1.25rem 0;
+    li {
+      label {
+        display: block;
+      }
+      input {
+        margin: .63rem 0;
+        width: 100%;
+        height: 1.56rem;
+      }
+      button {
+        display: block;
+        width: 5rem;
+        height: 1.56rem;
+        background: rgb(255, 82, 1);
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        outline: none;
+      }
+    }
+  }
+}
+</style>
