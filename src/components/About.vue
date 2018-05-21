dc1ddbde78eb19a3d49a774bb86278b0940ddd31
caishijian2008 <442754043@qq.com>
2018-05-22 00:46:17 +0800

    about 1

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <MHeader back="true">我的</MHeader>
+    <MHeader :back="true">我的</MHeader>
     <div class="content">
-      <section>
+      <section class="top-wrap">
         <div class="top">
-          <img src="" alt="">
-          <div>
+          <div class="top-main">
+            <img src="" alt="">
             <p>名字</p>
             <p>用户名：</p>
           </div>
@@ -37,26 +37,22 @@ export default {
 
 <style lang="scss" scoped>
 .content {
-  ul {
-    margin: 3.13rem 1.25rem 0;
-    li {
-      label {
-        display: block;
-      }
-      input {
-        margin: .63rem 0;
-        width: 100%;
-        height: 1.56rem;
-      }
-      button {
-        display: block;
-        width: 5rem;
-        height: 1.56rem;
-        background: rgb(255, 82, 1);
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        outline: none;
+  .top-wrap {
+    background: #fff;
+    margin-bottom: 15px;
+    padding: 10px;
+    .top {
+      overflow: hidden;
+      position: relative;
+      border-radius: 6px;
+      color: #fff;
+      background: linear-gradient(90deg, #eb3c3c, #ff7459);
+      box-shadow: 0 2px 5px rgba(255, 98, 98, .4);
+      .top-main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 25px 15px;
       }
     }
   }
